e5dd53457521d243b5ae7d7e5765cde6a6d2f53e
Richard <weichang321@gmail.com>
2018-01-20 10:55:21 +0800

    delete the spider's details.

--- parsing/cate_parsing.py
+++ parsing/cate_parsing.py
@@ -35,112 +35,19 @@ class GetTagAddr(object):
 
 
     def get_tag1_from(self, start_url):
-        try:
-            r = self.s.get(start_url, proxies=self.proxies, timeout=self.timeout)
-            tree = etree.HTML(r.text)
-            tag1_items = tree.xpath('//div[@id="classfy"]/a')
-            for i in tag1_items:
-                tag1 = i.getchildren()[0].text
-                url = i.attrib['href']
-                tag1_url_msg = {'tag1': tag1, 'url': url, 'status': 'ok'}
-                self.tag1_url_db.insert_one(tag1_url_msg)
-            self.linktime = 3    # 重置linktime
-            time.sleep(1)
-        except(requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-            if self.linktime > 0:
-                print('爬取失败，现在重新链接')
-                self.get_tag1_from(start_url)
-                self.linktime -= 1
-            else:
-                print('{}爬取失败'.format(start_url))
+        # 不公开爬虫细节
 
 
     def get_tag2_from(self, tag1_url):
-        tag1_url_msg = self.tag1_url_db.find_one({'url': tag1_url})
-        tag1 = tag1_url_msg['tag1']
-        try:
-            r = self.s.get(tag1_url, proxies=self.proxies, timeout=self.timeout)
-            tree = etree.HTML(r.text)
-            tag2_items = tree.xpath('//div[@id="classfy-sub"]/a')
-            if len(tag2_items) == 0:
-                tag2 = 'no_sub'
-                tag2_url_msg = {'tag1': tag1, 'tag2': tag2, 'url': tag1_url, 'status': 'tag1_not_sub'}
-                self.tag2_url_db.insert_one(tag2_url_msg)
-            else:
-                for i in tag2_items:
-                    tag2 = i.getchildren()[0].text
-                    url = i.attrib['href']
-                    tag2_url_msg = {'tag1': tag1, 'tag2': tag2, 'url': url, 'status': 'ok'}
-                    self.tag2_url_db.insert_one(tag2_url_msg)
-            self.crawly_tag1_ok.insert_one({'url': tag1_url})
-            self.linktime = 3    # 重置linktime
-            time.sleep(1)
-        except(requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-            if self.linktime > 0:
-                print('爬取失败，现在重新链接')
-                self.get_tag2_from(tag1_url)
-                self.linktime -= 1
-            else:
-                print('{}爬取失败'.format(tag1_url))
+        # 不公开爬虫细节
 
 
     def get_addr1_from(self, tag2_url):
-        tag2_url_msg = self.tag2_url_db.find_one({'url': tag2_url})
-        tag1 = tag2_url_msg['tag1']
-        tag2 = tag2_url_msg['tag2']
-        try:
-            r = self.s.get(tag2_url, proxies=self.proxies, timeout=self.timeout)
-            tree = etree.HTML(r.text)
-            addr1_items = tree.xpath('//div[@id="region-nav"]/a')
-            for i in addr1_items:
-                addr1 = i.getchildren()[0].text
-                url = i.attrib['href']
-                addr1_url_msg = {'tag1': tag1, 'tag2': tag2, 'addr1': addr1, 'url': url}
-                self.addr1_url_db.insert_one(addr1_url_msg)
-            self.crawly_tag2_ok.insert_one({'url': tag2_url})
-            self.linktime = 3  # 重置linktime
-            time.sleep(1)
-        except(requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout,
-               requests.exceptions.ConnectionError):
-            if self.linktime > 0:
-                print('爬取失败，现在重新链接')
-                self.get_addr1_from(tag2_url)
-                self.linktime -= 1
-            else:
-                print('{}爬取失败'.format(tag2_url))
+        # 不公开爬虫细节
 
 
     def get_addr2_from(self, addr1_url):
-        addr1_url_msg = self.addr1_url_db.find_one({'url': addr1_url})
-        tag1 = addr1_url_msg['tag1']
-        tag2 = addr1_url_msg['tag2']
-        addr1 = addr1_url_msg['addr1']
-        try:
-            r = self.s.get(addr1_url, proxies=self.proxies, timeout=self.timeout)
-            tree = etree.HTML(r.text)
-            addr2_items = tree.xpath('//div[@id="region-nav-sub"]/a')
-            if len(addr2_items) == 0:
-                addr2 = 'no_sub'
-                addr2_url_msg = {'tag1': tag1, 'tag2': tag2, 'addr1': addr1, 'addr2': addr2, 'url': addr1_url, 'status': 'addr1_not_sub'}
-                self.addr2_url_db.insert_one(addr2_url_msg)
-            else:
-                for i in addr2_items:
-                    addr2 = i.getchildren()[0].text
-                    url = i.attrib['href']
-                    addr2_url_msg = {'tag1':tag1, 'tag2': tag2, 'addr1':addr1, 'addr2': addr2, 'url':url}
-                    self.addr2_url_db.insert_one(addr2_url_msg)
-            self.crawly_addr1_ok.insert_one({'url': addr1_url})
-            self.linktime = 3  # 重置linktime
-            time.sleep(1)
-        except(
-        requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout,
-        requests.exceptions.ConnectionError):
-            if self.linktime > 0:
-                print('爬取失败，现在重新链接')
-                self.get_addr2_from(addr1_url)
-                self.linktime -= 1
-            else:
-                print('{}爬取失败'.format(addr1_url))
+        # 不公开爬虫细节
 
 
 if __name__ == '__main__':
--- parsing/shop_parsing.py
+++ parsing/shop_parsing.py
@@ -21,96 +21,15 @@ crawly_addr2_ok = dp['crawly_addr2_ok']  # 存储爬取成功的addr2_url
 
 
 def get_msg_from(response, addr2_url):
-    addr2_url_msg = addr2_url_db.find_one({'url': addr2_url})
-    tag1 = addr2_url_msg['tag1']
-    tag2 = addr2_url_msg['tag2']
-    addr1 = addr2_url_msg['addr1']
-    addr2 = addr2_url_msg['addr2']
-    tree = etree.HTML(response.text)
-    # 提取标题信息
-    title_items = tree.xpath('//div[@id="shop-all-list"]//div[@class="tit"]/a[1]')
-
-    # 提取商户星级
-    star_items = tree.xpath('//div[@class="comment"]/span')
-
-    # 提取评论数-人均价格
-    review_price_items = tree.xpath('//div[@class="comment"]/a')
-    review_price_list = []
-    for i in review_price_items:
-        # 判断评论数-价格的a标签是否存在子元素，如果存在，则在子元素中提取评论数-价格，否则评论数-价格都为“None”
-        if i.getchildren():
-            text = i.getchildren()[0].text
-            review_price_list.append(text)
-        else:
-            review_price_list.append("None")
-    review_price_list = [review_price_list[i:i + 2] for i in range(0, len(review_price_list), 2)]
-
-    # 提取各项评分
-    score_items = tree.xpath('//span[@class="comment-list"]/span/b')
-    score_items = [i.text for i in score_items]
-    score_items = [score_items[i:i + 3] for i in range(0, len(score_items), 3)]
-
-    # 提取菜系-商区-详细地址
-    tag_items = tree.xpath('//div[@class="tag-addr"]//span')
-    tag_items = [i.text for i in tag_items]
-    tag_items = [tag_items[i:i + 3] for i in range(0, len(tag_items), 3)]
-
-    for title_url, star, review_price, score, tag_area_addr in zip(title_items, star_items, review_price_list,
-                                                                   score_items, tag_items):
-        dpshop_msg = {
-            'title': title_url.attrib['title'],
-            'url': title_url.attrib['href'],
-            'star': star.attrib['title'],
-            'review': review_price[0],
-            'price': review_price[1],
-            'taste': score[0],
-            'env': score[1],
-            'service': score[2],
-            'tag1': tag1,
-            'tag2': tag2,
-            'addr1': addr1,
-            'addr2': addr2,
-            'full_addr': tag_area_addr[2]
-        }
-        dpshop.insert_one(dpshop_msg)
-    crawly_addr2_ok.insert_one({'url': addr2_url})
+    # 不公开爬虫细节
 
 
 def get_all_msg_from(addr2_url):
-    for page in range(1, PAGE_NUM_MAX+1):
-        result_url = '{}p{}'.format(addr2_url, page)
-        if not crawly_addr2_ok.find_one({'url': result_url}):
-            status_code, response = requests_url(result_url)
-            if status_code == 'link_bad':
-                print('请求失败，请在crawly_addr2_url_bad中查看请求失败url')
-                break
-            elif status_code == 404:
-                # addr2_bad = {'url': result_url, 'status': 404}
-                print('{}没有相关商户'.format(result_url))
-                break
-            else:
-                get_msg_from(response, addr2_url)
+    # 不公开爬虫细节
 
 
 def requests_url(result_url, linktime=LINKTIME):
-    s = requests.Session()
-    headers = random.choice(USER_AGENT)
-    s.headers.update({'User-Agent': headers})
-    proxies = {'http':random.choice(PROXY)}
-    try:
-        r = s.get(result_url, proxies=proxies, timeout=TIMEOUT)
-        time.sleep(1)
-        return (r.status_code, r)
-    except(requests.exceptions.ProxyError, requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout,
-           requests.exceptions.ConnectionError):
-        if linktime > 0:
-            print('请求失败，现在重新链接')
-            linktime -= 1
-            requests_url(result_url, linktime)
-        else:
-            status_code = 'link_bad'
-            response = None
-            return (status_code, response)
+    # 不公开爬虫细节
 
 
 if __name__ == '__main__':
